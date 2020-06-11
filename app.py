66aa6ff75c8790c1e2b0b087cecb0329f0e403bf
Jay Vigilla <JayRVigilla@gmail.com>
2020-06-11 00:34:18 -0700

    updated requirements.txt

--- app.py
+++ app.py
@@ -13,8 +13,8 @@ CURR_USER_KEY = "curr_user"
 app = Flask(__name__)
 
 
-app.config['SQLALCHEMY_DATABASE_URI'] = (
-    os.environ.get('DATABASE_URL', 'postgres:///warbler'))
+app.config['SQLALCHEMY_DATABASE_URI'] =
+    os.environ.get('DATABASE_URL', 'postgres:///warbler')
 
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_ECHO'] = False
